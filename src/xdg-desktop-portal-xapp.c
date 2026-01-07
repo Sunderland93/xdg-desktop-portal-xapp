df0b8eec27a9038a291265fe57d03d219af20138
Aleksey Samoilov <samoilov.lex@gmail.com>
2026-01-07 04:09:54 +0400

    handle screencast init

--- src/xdg-desktop-portal-xapp.c
+++ src/xdg-desktop-portal-xapp.c
@@ -49,6 +49,7 @@
 #include "lockdown.h"
 #include "request.h"
 #include "screenshot.h"
+#include "screencast.h"
 #include "settings.h"
 #include "wallpaper.h"
 
@@ -118,6 +119,12 @@ on_bus_acquired (GDBusConnection *connection,
   {
     return;
   }
+
+  if ((CINNAMON_MODE) && !screen_cast_init (connection, &error))
+    {
+      g_warning ("error: %s\n", error->message);
+      g_clear_error (&error);
+    }
 
   if ((CINNAMON_MODE || XFCE_MODE) && !screenshot_init (connection, &error))
     {
